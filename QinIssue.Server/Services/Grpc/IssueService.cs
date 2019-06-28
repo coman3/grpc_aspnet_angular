b29a56d740ac84dc5f984b2b5ca9ecd173121307
Lachlan van der Velden <me@lachlanvelden.com>
2019-06-28 13:04:00 +1000

    Initial Commit

---
+++ QinIssue.Server/Services/Grpc/IssueService.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using QinIssue.Models;
+using QinIssue.Server.Services;
+
+namespace QinIssue.Server.Services.Grpc
+{
+    public class IssueServiceImpl : IssueService.IssueServiceBase
+    {
+
+        private IssueRepository repo;
+
+        public IssueServiceImpl(IssueRepository repo)
+        {
+            this.repo = repo;
+        }
+        public override Task<BoolStatus> CreateIssue(NewIssue request, ServerCallContext context)
+        {
+            try
+            {
+                var issue = new Issue();
+                issue.Id = Guid.NewGuid().ToString();
+                issue.Title = request.Title;
+                repo.AddIssue(issue);
+                return Task.FromResult(new BoolStatus { Completed = true });
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(new BoolStatus { Completed = false });
+            }
+        }
+
+        public override Task<IssueList> GetIssues(GetIssue request, ServerCallContext context)
+        {
+            var issues = repo.GetIssues().Where(x => x.Title.ToLower().Contains(request.TitleContains.ToLower()));
+            var issuesList = new IssueList();
+            issuesList.Issues.Add(issues);
+            return Task.FromResult(issuesList);
+        }
+    }
+}
---
+++ QinIssue.Server/Services/IssueDatastore.cs
@@ -0,0 +1,24 @@
+﻿using QinIssue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QinIssue.Server.Services
+{
+    public class IssueRepository
+    {
+        private List<Issue> Issues = new List<Issue>();
+
+
+        public List<Issue> GetIssues()
+        {
+            return Issues;
+        }
+
+        public void AddIssue(Issue issue)
+        {
+            Issues.Add(issue);
+        }
+    }
+}
