b8743ce4174f674d3903e8885b9eead37992ea6d
Lachlan van der Velden <me@lachlanvelden.com>
2019-06-28 13:12:39 +1000

    Add Issue page and remove bloat from app component

--- QinIssue.Frontend/src/app/app-routing.module.ts
+++ QinIssue.Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IssueComponent } from './issue/issue.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'issues', component: IssueComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- QinIssue.Frontend/src/app/app.module.ts
+++ QinIssue.Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueComponent
   ],
   imports: [
     BrowserModule,
